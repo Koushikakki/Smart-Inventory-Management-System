0f59d7d2a445224d07feefec59b6041b004b627f
admin <admin@MacBookPro.lan>
2025-08-01 18:00:05 +0530

    add feature to display all the products'

--- src/ManageProduct/productManager.ts
+++ src/ManageProduct/productManager.ts
@@ -14,4 +14,30 @@ export function addProduct(name : string, category :string, stock : number, supp
   console.log(` Product '${name}' added.`);
 }
 
+export function listProducts(): void {
+  console.log(`\n Product List:`);
+  products.forEach((p) =>
+    console.log(`- ${p.name} [${p.category}] | Stock: ${p.stock}`)
+  );
+}
 
+export function showProductDetails(name: string): void {
+  const product = products.find((p) => p.name.toLowerCase() === name.toLowerCase());
+  if (!product) return console.log(` Product not found with name '${name}'.`);
+  
+  console.log(`\n Product Details:
+Name: ${product.name}
+Category: ${product.category}
+Stock: ${product.stock}`);
+
+  if (product.supplierId) {
+    const supplier = suppliers.find((s) => s.id === product.supplierId);
+    if (supplier) {
+      console.log(`Supplier: ${supplier.name}`);
+    }
+  }
+}
+
+
+
+    
